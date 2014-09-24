bf06a2ead0db0ae01d28e9a777888934fa33fc74
Jort van Breda <jort@onlinetouch.nl>
2014-09-24 11:19:41 +0200

    Zwart 2

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject zwart "0.1.3"
+(defproject zwart "2"
   :description "Noir and Fleet extensions"
   :url "https://github.com/touch/zwart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ org.clojure/clojure              "1.5.1"  ]
                  [ org.clojars.touch/fleet          "0.10.2" ]
-                 [ org.clojars.touch/noir 			"1.3.0" :exclusions [org.clojure/clojure]]])
+                 [ org.clojars.touch/noir 			"1.3.1" :exclusions [org.clojure/clojure]]])
