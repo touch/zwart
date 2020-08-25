c6f4ea8803bfe525abad2f2a6473a8a3abbe4618
Misha Vakulich <m.vakulich@softteco.com>
2020-08-26 02:40:07 +0300

    up lib

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ org.clojure/clojure              "1.5.1"  ]
-                 [ org.clojars.touch/fleet          "0.10.2" ]
+                 [ org.clojars.touch/fleet          "0.10.4" ]
                  [ org.clojars.touch/noir 			"1.3.1" :exclusions [org.clojure/clojure]]])
