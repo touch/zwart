f1ce5a69ed7ee44de9c50e6178f06ac3f3afb6e8
Misha Vakulich <m.vakulich@softteco.com>
2020-08-26 22:38:53 +0300

    move to nexus

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/touch/zwart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"jenkins.onlinetouch.eu" "https://jenkins.onlinetouch.eu/plugin/repository/everything/"}
+  :repositories {"reposilite.onlinetouch.eu" "https://reposilite.onlinetouch.eu/repository/onlinetouch/"}
   :dependencies [[ org.clojure/clojure              "1.5.1"  ]
                  [ org.clojars.touch/fleet          "0.10.5-SNAPSHOT" ]
                  [ org.clojars.touch/noir 			"1.3.2-SNAPSHOT" :exclusions [org.clojure/clojure]]])
