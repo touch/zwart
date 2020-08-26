3ad86d1379995d3df6a0481f27ab529f2ae41947
Misha Vakulich <m.vakulich@softteco.com>
2020-08-26 11:54:42 +0300

    fix jenkins pipeline

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,7 +3,7 @@ pipeline {
   stages {
     stage('Build') {
       steps {
-        lein install
+        sh 'lein install'
       }
     }
   }
--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/touch/zwart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"jenkins.onlinetouch.eu" "https://jenkins.onlinetouch.eu/plugin/repository/everything/"}
   :dependencies [[ org.clojure/clojure              "1.5.1"  ]
                  [ org.clojars.touch/fleet          "0.10.4" ]
                  [ org.clojars.touch/noir 			"1.3.1" :exclusions [org.clojure/clojure]]])
