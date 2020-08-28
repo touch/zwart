6a8360457ad1f062f09c6db14b443c672565cfaa
Misha Vakulich <m.vakulich@softteco.com>
2020-08-28 23:19:33 +0300

    fix build script

--- project.clj
+++ project.clj
@@ -3,7 +3,15 @@
   :url "https://github.com/touch/zwart"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"reposilite.onlinetouch.eu" "https://reposilite.onlinetouch.eu/repository/onlinetouch/"}
   :dependencies [[ org.clojure/clojure              "1.5.1"  ]
                  [ org.clojars.touch/fleet          "0.10.5-SNAPSHOT" ]
-                 [ org.clojars.touch/noir 			"1.3.2-SNAPSHOT" :exclusions [org.clojure/clojure]]])
+                 [ org.clojars.touch/noir 			"1.3.2-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.23"
+                 {:extensions "true"
+                  :configuration ([:sourceDirectories [:sourceDirectory "src"]]
+                                  [:temporaryOutputDirectory "true"]
+                                  [:vmargs "-Xmx256m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled"])
+                  :executions [:execution
+                               [:id "compile-clojure"]
+                               [:phase "compile"]
+                               [:goals [:goal "compile"]]]}]])
